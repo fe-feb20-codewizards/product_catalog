78afcd0ac9894e15c9ddb2308a2e9a96d3e6344d
Daniel <henio09@wp.pl>
2023-06-12 18:04:31 +0200

    added app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/App.scss';
+
+import Footer from './components/Footer.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+
+      <main></main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
