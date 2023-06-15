3cb89867cd80005484171b949f8c3e5947868dfb
aliksandr porakh <sashapohh@gmail.com>
2023-06-15 11:08:57 +0200

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,70 +17,64 @@ export const Header = () => {
 	return (
 		<div className="navigation-header__left">
 			<BurgerPopUp />
+			
 			<ul className="navigation-header__list_left">
-				<li className="navigation-header__item logo">
-					<NavLink
-						to="/"
-						className="navigation-header__link__logo is-active-link"
-					>
-						<img
-							className="navigation-header__image"
-							src={process.env.PUBLIC_URL + '/images/logo.svg'}
-							alt="logo"
-						/>
-					</NavLink>
-				</li>
-				<li className="navigation-header__item_l">
-					<NavLink
-						to="/"
-						className={({ isActive }) =>
-							classNames('navigation-header__link', { active: isActive })
-						}
-					>
+				<div className='navigation-header__cotainer'>
+					<li className="navigation-header__item logo">
+						<NavLink
+							to="/"
+							className="navigation-header__link__logo is-active-link"
+						>
+							<img
+								className="navigation-header__image"
+								src={process.env.PUBLIC_URL + '/images/logo.svg'}
+								alt="logo"
+							/>
+						</NavLink>
+					</li>
+				
+					<li className="navigation-header__item_l">
+						<NavLink
+							to="/"
+							className={({ isActive }) =>
+								classNames('navigation-header__link', { active: isActive })
+							}
+						>
 						Home
-					</NavLink>
-				</li>
-				<li className="navigation-header__item_l">
-					<NavLink
-						to="/phones"
-						className={({ isActive }) =>
-							classNames('navigation-header__link', { active: isActive })
-						}
-					>
+						</NavLink>
+					</li>
+					<li className="navigation-header__item_l">
+						<NavLink
+							to="/phones"
+							className={({ isActive }) =>
+								classNames('navigation-header__link', { active: isActive })
+							}
+						>
 						Phones
-					</NavLink>
-				</li>
-				<li className="navigation-header__item_l">
-					<NavLink
-						to="/tablets"
-						className={({ isActive }) =>
-							classNames('navigation-header__link', { active: isActive })
-						}
-					>
+						</NavLink>
+					</li>
+					<li className="navigation-header__item_l">
+						<NavLink
+							to="/tablets"
+							className={({ isActive }) =>
+								classNames('navigation-header__link', { active: isActive })
+							}
+						>
 						Tablets
-					</NavLink>
-				</li>
-				<li className="navigation-header__item_l">
-					<NavLink
-						to="/accessories"
-						className={({ isActive }) =>
-							classNames('navigation-header__link', { active: isActive })
-						}
-					>
+						</NavLink>
+					</li>
+					<li className="navigation-header__item_l">
+						<NavLink
+							to="/accessories"
+							className={({ isActive }) =>
+								classNames('navigation-header__link', { active: isActive })
+							}
+						>
 						Accessories
-					</NavLink>
-				</li>
+						</NavLink>
+					</li>
+				</div>
 				<div className="nav-buttons">
-					<Link
-						to="/favorites"
-						className="nav-buttons__element block"
-					>
-						<img
-							src={process.env.PUBLIC_URL + '/images/Shopping bag (Cart).svg'}
-							alt="favorities icon"
-							className="nav-buttons__icon"
-						/>
-					</Link>
 					<Link
 						to="/cart"
 						className="nav-buttons__element block"
@@ -91,6 +85,17 @@ export const Header = () => {
 							className="nav-buttons__icon"
 						/>
 					</Link>
+					<Link
+						to="/favorites"
+						className="nav-buttons__element block"
+					>
+						<img
+							src={process.env.PUBLIC_URL + '/images/Shopping bag (Cart).svg'}
+							alt="favorities icon"
+							className="nav-buttons__icon"
+						/>
+					</Link>
+					
 					<a
 						href={linkHref}
 						className={`nav-buttons__element none ${isActive ? 'lineactive' : ''
