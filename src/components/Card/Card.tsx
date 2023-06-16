5dc57c4edf69609e966b2401336a7718062cddc1
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-06-16 10:37:08 +0200

    Update link in card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -31,9 +31,9 @@ export default function Card({ phone }: CardProps) {
 	};
 
 	return (
-		<Link className="no-underline" to={`/item/${itemId}`}>
-			<div className="card">
-				<div className="card__container">
+		<div className="card">
+			<div className="card__container">
+				<Link className="no-underline" to={`/item/${itemId}`}>
 					<div className="card__img">
 						<img
 							className="card__img-photo"
@@ -42,51 +42,53 @@ export default function Card({ phone }: CardProps) {
 						>
 						</img>
 					</div>
-					<div className="card__wrapper">
+				</Link>
+				<div className="card__wrapper">
+					<Link className="no-underline" to={`/item/${itemId}`}>
 						<div className="card__title">
 							{name}
 						</div>
-						<div className="card__price">
-							{price !== fullPrice && <h4 className="card__price-head">{price}</h4>}
-							<h4 className="card__price-discounted">{fullPrice}</h4>
+					</Link>
+					<div className="card__price">
+						{price !== fullPrice && <h4 className="card__price-head">{price}</h4>}
+						<h4 className="card__price-discounted">{fullPrice}</h4>
+					</div>
+
+					<div className="card__divider"></div>
+
+					<ul className="card__info">
+						<div className="card__info-left">
+							<li>Screen</li>
+							<li>Capacity</li>
+							<li>RAM</li>
 						</div>
+						<div className="card__info-right">
+							<li>{screen}</li>
+							<li>{capacity}</li>
+							<li>{ram}</li>
+						</div>
+					</ul>
 
-						<div className="card__divider"></div>
-
-						<ul className="card__info">
-							<div className="card__info-left">
-								<li>Screen</li>
-								<li>Capacity</li>
-								<li>RAM</li>
-							</div>
-							<div className="card__info-right">
-								<li>{screen}</li>
-								<li>{capacity}</li>
-								<li>{ram}</li>
-							</div>
-						</ul>
-
-						<div className="card__buttons">
-							<button 
-								className="card__buttons-add-to-cart"
-								onClick={() => addToCart(phone)}
-							>
+					<div className="card__buttons">
+						<button 
+							className="card__buttons-add-to-cart"
+							onClick={() => addToCart(phone)}
+						>
 								Add to cart
-							</button>
-							<button 
-								className="card__buttons-add-to-favorites"
-								onClick={handleFavoriteClick}
-							>	
-								<img
-									className="card__buttons-add-to-favorites-icon"
-									src={process.env.PUBLIC_URL + (isFavorite ? '/images/heart-filled.svg' : '/images/heart.svg')}
-									alt="Add to favorites"
-								/>
-							</button>
-						</div>
+						</button>
+						<button 
+							className="card__buttons-add-to-favorites"
+							onClick={handleFavoriteClick}
+						>	
+							<img
+								className="card__buttons-add-to-favorites-icon"
+								src={process.env.PUBLIC_URL + (isFavorite ? '/images/heart-filled.svg' : '/images/heart.svg')}
+								alt="Add to favorites"
+							/>
+						</button>
 					</div>
 				</div>
 			</div>
-		</Link>
+		</div>
 	);
 }
