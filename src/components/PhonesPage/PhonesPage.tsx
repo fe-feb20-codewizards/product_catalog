ef72389224d76a986f729caea434d71566390a10
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-06-16 16:58:03 +0200

    PhonesPage

--- src/components/PhonesPage/PhonesPage.tsx
+++ src/components/PhonesPage/PhonesPage.tsx
@@ -36,7 +36,7 @@ export default function PhonesPage() {
 	const { currentCardPag, onPageChange, startingCard, endingCard, firstPage, lastPage } = page;
 
 	const showingCards = sortedPhones.slice(startingCard - 1, endingCard);
-	
+
 	const handleButtonPrev = useCallback(() => {
 		if (activeButton > 1 && currentCardPag > 1) {
 			onPageChange(currentCardPag - 1);
@@ -45,7 +45,7 @@ export default function PhonesPage() {
 	}, [activeButton]);
 
 	const handleButtonNext = useCallback(() => {
-		if (activeButton < maxPages && currentCardPag < maxPages) {
+		if (currentCardPag < maxPages) {
 			onPageChange(currentCardPag + 1);
 			onChanger(activeButton + 1);
 		}
@@ -67,7 +67,9 @@ export default function PhonesPage() {
 					<div>
 						<p className='phone__sorting-text'>Sort by</p>
 						<label htmlFor="sort" >
-							<select name="sort" id="sort" value={sort || 'Choose sort'} onChange={handleSort} className='phones__sorting-sortby' >
+							<select
+								name="sort" id="sort" value={sort || 'Choose sort'} onChange={handleSort} 
+								className='phones__sorting phones__sorting-sortby' >
 								<option value="Newest">Newest</option>
 								<option value="PriceUp">Price Up</option>
 								<option value="PriceDown">Price Down</option>
@@ -77,29 +79,43 @@ export default function PhonesPage() {
 					<div>
 						<p className='phone__sorting-text'>Items on page</p>
 						< label htmlFor="page" className='phones__sorting-page'>
-							<select name="page" id="page" value={perPage}onChange={handlePerpage} className='phones__sorting-pages'>
+							<select	
+								name="page" id="page" value={perPage} onChange={handlePerpage} 
+								className='phones__sorting phones__sorting-pages'>
 								<option value="4">4</option>
 								<option value="16">16</option>
 								<option value="20">20</option>
 							</select></label></div>
 
 				</div>
-				<article className='phones__cards'>
-					<div className='phones__cards phones__cards-card'>
-						{showingCards.map(phone => <Card phone={phone} key={phone.id} />)}
-					</div>
-				</article>
-				<footer className='phones___footer'>
-					<div className='phones__footer-button'>
-						<button onClick={handleButtonPrev} className={`${firstPage && 'phones__footer__disabled'}`}>{'<'}</button>
+
+				<div className='phones___buttons'>
+					<div className='phones__buttons__container'>
+						<button onClick={handleButtonPrev} className={`phones__buttons__container-button ${firstPage && 'phones__buttons__container-button--disabled'}`}>{'<'}</button>
 						{getNumbers(firstButton, lastButton + 1).map(page => <Button
 							key={page}
 							page={page}
 							current={currentCardPag}
 							handlePage={handlePage} />)}
-						<button onClick={handleButtonNext} className={`${lastPage && 'phones__footer__disabled'}`}>{'>'}</button>
+						<button onClick={handleButtonNext} className={`phones__buttons__container-button ${lastPage && 'phones__buttons__container-button--disabled'}`}>{'>'}</button>
 					</div>
-				</footer>
+				</div>
+				<article className='phones__cards'>
+					<div className='phones__cards phones__cards-card'>
+						{showingCards.map(phone => <Card phone={phone} key={phone.id} />)}
+					</div>
+				</article>
+				<div className='phones___buttons'>
+					<div className='phones__buttons__container'>
+						<button onClick={handleButtonPrev} className={`phones__buttons__container-button ${firstPage && 'phones__buttons__container-button--disabled'}`}>{'<'}</button>
+						{getNumbers(firstButton, lastButton + 1).map(page => <Button
+							key={page}
+							page={page}
+							current={currentCardPag}
+							handlePage={handlePage} />)}
+						<button onClick={handleButtonNext} className={`phones__buttons__container-button ${lastPage && 'phones__buttons__container-button--disabled'}`}>{'>'}</button>
+					</div>
+				</div>
 			</section>
 		</div>
 	);
