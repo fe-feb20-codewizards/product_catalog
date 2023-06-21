488047c8cb490a482fb5fd5a76cb51c86b39c871
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-06-21 06:42:44 +0200

    loader

--- src/components/Features/Loader/Loader.tsx
+++ src/components/Features/Loader/Loader.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CirclesWithBar } from 'react-loader-spinner';
 
 export function Loader() {
+	const [isLoader, setIsLoader] = useState(true);
+
+	setTimeout(() => setIsLoader(false), 15000);
+
 	return (
-		<div style={{ width: '400px', margin: 'auto', display: 'flex', gap: '35px', paddingTop: '50px' }}>
-			<h1>Loading</h1>
-			<CirclesWithBar
-				height="100"
-				width="100"
-				color="#4fa94d"
-				wrapperStyle={{}}
-				wrapperClass=""
-				visible={true}
-				outerCircleColor=""
-				innerCircleColor=""
-				barColor=""
-				ariaLabel='circles-with-bar-loading'
-			/>
-		</div>);
+		< div style={{ width: '500px', margin: 'auto', display: 'flex', gap: '35px', paddingTop: '50px' }} >
+			{isLoader
+				? (
+					<><h1>Loading</h1><CirclesWithBar
+						height="100"
+						width="100"
+						color="#4fa94d"
+						wrapperStyle={{}}
+						wrapperClass=""
+						visible={true}
+						outerCircleColor=""
+						innerCircleColor=""
+						barColor=""
+						ariaLabel='circles-with-bar-loading' /></>
+				)
+				: (
+					<h1>Server no response</h1>)
+			}</div >);
 }
--- src/components/TabletsPage/TabletsPage.tsx
+++ src/components/TabletsPage/TabletsPage.tsx
@@ -3,11 +3,13 @@ import { useCatalogContext } from '../CatalogContext';
 // import ListProduct from '../ListProducts/ListProduct';
 // import { Link } from 'react-router-dom';
 import './TabletsPage.scss';
+import { Loader } from '../Features/Loader/Loader';
 
 export function TabletsPage() {
 	// const { phonesData } = useCatalogContext();
 	return (
 		<div className='tablets'>
+			<Loader />
 			{/* <div className='tablets__links'>
 				<Link to="/product_catalog/" className="tablets__link">
 					<img src={process.env.PUBLIC_URL + '/images/Home.jpg'} alt="" className='tablets__home'/>
@@ -22,7 +24,7 @@ export function TabletsPage() {
 			<section>
 				<ListProduct list={phonesData} />  
 			</section> */}
-			<h1 style={{fontSize: '50px', textAlign: 'center'}}>Page in building process!!!</h1>
+			{/* <h1 style={{fontSize: '50px', textAlign: 'center'}}>Page in building process!!!</h1> */}
 		</div>
 	);
 }
